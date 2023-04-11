b13952deaf48a5a232a09501ce4c87d3bdf174c5
Jordan Dare <jordandare@hotmail.com>
2023-04-11 15:06:00 -0700

    fix boost deprecation warning

--- source/main.h
+++ source/main.h
@@ -42,7 +42,7 @@ enum {
 };
 
 #include <iostream>
-#include <boost/bind.hpp>
+#include <boost/bind/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
