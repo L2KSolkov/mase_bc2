fdaa3ccb5b525954bc5ece4f59e03f3d82bebfc6
Jordan Dare <jordandare@hotmail.com>
2023-04-11 16:05:12 -0700

    add setup script for submodules

---
+++ Scripts/Setup.bat
@@ -0,0 +1,7 @@
+@echo off
+rem this script is to setup the boost submodule + headers. you must run this before using the project.
+cd ..
+git submodule update --init --recursive
+cd external\boost
+bootstrap.bat
+b2 headers
